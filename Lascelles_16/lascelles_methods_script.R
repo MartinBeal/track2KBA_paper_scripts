d28805e4237626549c13d46594bc0eee28054a92
Bealhammar <mbeal@ispa.pt>
2019-02-11 15:11:33 +0000

    Run-through, with small edits to improve readability and simplify examples.

--- Lascelles_16/lascelles_methods_script.R
+++ Lascelles_16/lascelles_methods_script.R
@@ -3,7 +3,7 @@
 ## Downloaded: 25-09-2018 by Martin Beal 
 
 
-############ data preparation ##############
+## Packages and source script prep ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
 
 source("Lascelles_16/source_scripts(update_06.18).r")
 
@@ -12,30 +12,29 @@ kpacks <- c('sp','maptools', 'rgdal', 'adehabitatHR','geosphere','fields','spats
 new.packs <- kpacks[!(kpacks %in% installed.packages()[,"Package"])]
 
 if(length(new.packs)) install.packages(new.packs)
-lapply(kpacks, require, character.only=T)
+lapply(kpacks, require, character.only = T)
 remove(kpacks, new.packs)
+
+## Data preparation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+
 DataGroup <- read.csv("Lascelles_16/lascelles_test.data.csv")
- # replace "table.csv" by the name of your table with tracking data (see above)
 
-# to see the data
 head(DataGroup)
-plot(Latitude~Longitude, data=DataGroup, asp=1)
-map("world", add=T)
+plot(Latitude ~ Longitude, data = DataGroup, asp = 1)
+map("world", add = T)
 
-# calculate TrackTime field
-DataGroup$DateTime <- paste(DataGroup$DateGMT, DataGroup$TimeGMT, sep= " ")
-DataGroup$DateTime <- as.POSIXct(strptime(DataGroup$DateTime, "%d/%m/%Y %H:%M:%S "), "GMT") ##see other options for date format in strptime help
-DataGroup$TrackTime <- as.numeric(DataGroup$DateTime)
+DataGroup$DateTime <- paste(DataGroup$DateGMT, DataGroup$TimeGMT, sep =  " ") # calculate TrackTime field
+DataGroup$DateTime <- as.POSIXct(strptime(DataGroup$DateTime, "%d/%m/%Y %H:%M:%S "), "GMT")
+DataGroup$TrackTime <- as.numeric(DataGroup$DateTime) # numberic time field 
 head(DataGroup)
 
-# create colony data.frame
-Colony <- data.frame(Longitude = -38.06, Latitude = -54) # replace by Longitude and Latitude values of the colony
+Colony <- data.frame(Longitude = -38.06, Latitude = -54) # create colony location data.frame
 #or
 #Colony=read.csv("nests.csv") # a data.frame with a Latitude and Longitude value per individual ID nest
-# see individual tracks and colony location
-Tracks <- unique(DataGroup$ID)
+
+Tracks <- unique(DataGroup$ID) # see individual tracks and colony location
 DataGroup$TrackId <- as.factor(DataGroup$ID)
-plot(Latitude~Longitude, data=DataGroup, col="white", asp=1)
+plot(Latitude ~ Longitude, data = DataGroup, col = "white", asp = 1)
 for(i in Tracks)
 {
   TempTrack <- subset(DataGroup, DataGroup$TrackId == i)
@@ -46,16 +45,19 @@ map("world", add=T)
 with(Colony, points(Longitude, Latitude, pch=19, cex=3))
 
 
-#### split the tracks into trips and removes locations on the colony ####
+## TripSplit function: Split tracks into trips based on colony proximity ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+
 DataGroup.Df <- DataGroup
-DataGroup.Wgs <- SpatialPoints(data.frame(DataGroup$Longitude, DataGroup$Latitude), proj4string=CRS("+proj=longlat + datum=wgs84"))
-DgProj <- CRS(paste("+proj=laea +lon_0=", Colony$Longitude, " +lat_0=", Colony$Latitude, sep=""))
-DataGroup.Projected <- spTransform(DataGroup.Wgs, CRS=DgProj)
+DataGroup.Wgs <- SpatialPoints(data.frame(DataGroup$Longitude, DataGroup$Latitude), proj4string = CRS("+proj=longlat + datum=wgs84"))
+DgProj <- CRS(paste("+proj=laea +lon_0=", Colony$Longitude, " +lat_0=", Colony$Latitude, sep = ""))
+DataGroup.Projected <- spTransform(DataGroup.Wgs, CRS = DgProj)
 DataGroup <- SpatialPointsDataFrame(DataGroup.Projected, data = DataGroup)
 DataGroupTrips <- NULL
+
 for(i in Tracks)
 {
   TempTrack <- subset(DataGroup, DataGroup$TrackId == i)
+  # 
   TempTrips <- tripSplit(Track = TempTrack, Colony = Colony, InnerBuff = 15, ReturnBuff = 45, plotit=T ,MidPoint = F, nests=F)
   if(which(Tracks == i) == 1)
   {
@@ -65,34 +67,38 @@ for(i in Tracks)
 
 names(DataGroupTrips@data)[names(DataGroupTrips@data) %in% c("DataGroup.Longitude" , "DataGroup.Latitude")] <- c("X", "Y")
 
-DataGroupTrips <- DataGroupTrips[DataGroupTrips$Returns != "N",]
+DataGroupTrips <- DataGroupTrips[DataGroupTrips$Returns != "N",] # Remove non-trips and unfinished trips
 DataGroupTrips <- DataGroupTrips[DataGroupTrips$trip_id != "-1",]
-# MB # remove trips with less than 5 points
-DataGroupTrips <- DataGroupTrips[!DataGroupTrips$trip_id %in% names(which(table(DataGroupTrips$trip_id) < 5)),]
+DataGroupTrips <- DataGroupTrips[!DataGroupTrips$trip_id %in% names(which(table(DataGroupTrips$trip_id) < 5)), ] # remove trips with less than 5 points
 head(DataGroupTrips@data)
 DataGroupTrips$originalID <- DataGroupTrips$ID #save the original IDs with a new name
 DataGroupTrips$ID <- DataGroupTrips$trip_id #reset the ID field to individual trips rather than individual birds
 
-# summary of the trip data
+
+## tripSummary: take tripSplit output and summarize the movements ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
 tsum <- tripSummary(DataGroupTrips, Colony=Colony, nests=F)
 tsum[order(tsum$departure),]
 
 
-######### identify the appropriate ARS Scale #########
+## scaleARS: identify scale of interaction with the environment ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
 
-Scales <- seq(10,250,5)
-fpt.scales <- scaleARS(DataGroup=DataGroupTrips,Scales=Scales,Peak="Flexible")
+Scales <- seq(10, 250, 5)
+fpt.scales <- scaleARS(DataGroup=DataGroupTrips, Scales=Scales, Peak="Flexible") # different peak-selecting options available
+# fpt.scales <- scaleARS(DataGroup=DataGroupTrips, Scales=Scales, Peak="First")
+# fpt.scales <- scaleARS(DataGroup=DataGroupTrips, Scales=Scales, Peak="Max")
+# fpt.scales <- scaleARS(DataGroup=DataGroupTrips, Scales=Scales, Peak="User")
 
-####### calculate the kernels and export the resultant polygons as shapefiles ########
+
+####### batch UD: calculate the kernels and export the resultant polygons as shapefiles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 datagroupsUDd <- batchUD(DataGroupTrips, Scale=fpt.scales/2, UDLev=50)
 
-td <- "output" # path to the folder where the shapefile will be saved
-writeOGR(datagroupsUDd, td, "testUD", driver="ESRI Shapefile") #MB# switched 'td' and '"testUD"' to send file to folder
-datagroupsUDd <- readOGR(td, "testUD", encoding="ESRI Shapefile")
+# td <- "output" # path to the folder where the shapefile will be saved
+# writeOGR(datagroupsUDd, td, "testUD", driver="ESRI Shapefile") #MB# switched 'td' and '"testUD"' to send file to folder
+# datagroupsUDd <- readOGR(td, "testUD", encoding="ESRI Shapefile")
 
 
-if ("originalID" %in% names(DataGroupTrips@data))
+if ("originalID" %in% names(DataGroupTrips@data)) 
 {
   TableKernel <- datagroupsUDd@data
   linkids <- with(DataGroupTrips@data,aggregate(ID, list(ID=ID,originalID=originalID), length))
@@ -102,10 +108,12 @@ if ("originalID" %in% names(DataGroupTrips@data))
 }
 datagroupsUDd@data
 
-###### variance test #######
+
+###### varianceTest: testing for site fidelity (i.e. space use pseudoreplication) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #######
 bird_string <- as.character(datagroupsUDd$originalID)
 vt <- varianceTest(datagroupsUDd, bird_string, Iteration=10)
 vt
+
 ### to choose randomly just one trip per individual
 if (vt < 0.25)
 {
@@ -119,15 +127,29 @@ if (vt < 0.25)
   datagroupsUDd=datagroupsUDd2
   DataGroupTrips=DataGroupTrips2
 }
-# Bootstrap
-# boot_out<-bootstrap(DataGroupTrips, Scale=fpt.scales/2, Iteration=50)
-# boot_out
 
-# polygons count
-count_UD <- polyCount(datagroupsUDd, Res = 0.05)
+
+## Bootstrap: estimate a trend lind for the relationship between sample size and area use  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+
+str(DataGroupTrips)
+
+##MB Take small subsample of data to test function
+DataGroupDf <- DataGroupTrips@data
+str(DataGroupDf)
+DataGroupSample <- DataGroupDf[DataGroupDf$ID==unique(DataGroupDf$ID)[1:4], ]
+
+DataGroupSample <- as.data.frame(DataGroupSample %>% dplyr::group_by(ID) %>% dplyr::filter(dplyr::row_number() <= 100))
+
+before <- Sys.time()
+boot_out <- bootstrap(DataGroupSample, Scale=fpt.scales/2, Iteration=10)
+boot_out
+Sys.time() - before
+
+## polyCount: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+count_UD <- polyCount(datagroupsUDd, Res = 0.1)
 writeRaster(count_UD, "output/count_UD_ras.tif")
 
-# Threshold_raster
+## Threshold_raster: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
 mIBA_site <- thresholdRaster(count_UD, Threshold = 15)
 writeOGR(mIBA_site, layer="spcies_mIBA_candidatesite_thresh.10p", dsn="output", driver="ESRI Shapefile", verbose=TRUE)
 
